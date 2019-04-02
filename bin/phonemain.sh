2ecb869926313599bed98bb7b2a56a90ae5c46af
qinweijunlin <qinweijunlin@qq.com>
2019-04-02 22:56:01 +0800

    发布

--- bin/phonemain.sh
+++ bin/phonemain.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 #
-# 控制手机的 log
+# 控制手机
 set -euo pipefail
 
-readonly ROOTDIR='/yzaj'
-readonly bindir="${ROOTDIR}/phone/bin"
-readonly tempdir="${ROOTDIR}/temp/phone"
-todaydir="${tempdir}/log/$(date +'%Y-%m-%d')"
-beforedir="${tempdir}/log/$(date +'%Y-%m-%d' -d '-7day')"
-readonly todaydir
-readonly beforedir
-phonelog="${todaydir}/$(date +'%H-%M-%S').log"
-readonly phonelog
 
-if [[ -d "${beforedir}" ]]; then
-  rm -r "${beforedir}"
-fi
 
-mkdir -p "${todaydir}"
 
-. "${bindir}"/phonemain.sh 2>&1 | tee "${phonelog}"
+
+
+
+
+
+
+
+
+
+
+
+
+
