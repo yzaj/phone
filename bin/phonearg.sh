dcb92736e4ece2d3d17e709faf9050ec05aea461
qinweijunlin <qinweijunlin@qq.com>
2019-04-04 07:10:19 +0800

    发布

--- bin/phonearg.sh
+++ bin/phonearg.sh
@@ -13,8 +13,10 @@ readonly E_UNRECOGNIZED_OPTION=2
 
 #### 主体 ####
 # USAGE: phone.sh all 101-118 121-124 128 130 reboot,sleep:30,home-game
-
-
+if (($# < 2)); then
+  err "Missing operand"
+  exit "${E_MISSING_OPERAND}"
+fi
 
 
 
