def7a38a1e8d4b458ffe9860c50b5ae5a5191c55
qinweijunlin <qinweijunlin@qq.com>
2019-04-04 06:33:51 +0800

    发布

--- bin/phonearg.sh
+++ bin/phonearg.sh
@@ -3,6 +3,23 @@
 # 控制手机的参数
 set -euo pipefail
 
+#### 常量 ####
+readonly E_MISSING_OPERAND=1
+readonly E_UNRECOGNIZED_OPTION=2
+
+#### 函数 ####
+
+#### 变量 ####
+
+#### 主体 ####
+
+
+
+
+
+
+
+
 phonenum=''
 phonecmd=''
 
