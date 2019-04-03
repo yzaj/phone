66ab234b5af929c6a92fa91b89ee5c7e5f366c31
qinweijunlin <qinweijunlin@qq.com>
2019-04-04 07:20:23 +0800

    发布

--- bin/phonearg.sh
+++ bin/phonearg.sh
@@ -18,9 +18,16 @@ if (($# < 2)); then
   exit "${E_MISSING_OPERAND}"
 fi
 
-
-
-
+for phonearg in "$@"; do
+  
+  if [[ "${phonearg}" == "all" ]]; then
+  :
+  else
+    err "Unrecognized option '${phonearg}'"
+    exit "${E_UNRECOGNIZED_OPTION}"
+  fi
+  
+done
 
 
 
