b2ee33f984d69d4f6c64e7d0705547f4bb2870f3
qinweijunlin <qinweijunlin@qq.com>
2019-04-03 08:35:44 +0800

    发布

--- bin/phonemain.sh
+++ bin/phonemain.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #
-# 控制手机
+# 控制手机的主程序
 set -euo pipefail
 
 #### 常量 ####
-
-##############
-err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
-}
 
 #### 包含 ####
 . "${confdir}"/phone.conf
