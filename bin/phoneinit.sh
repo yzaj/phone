c9baf78196e20810cd98d4a83a72b56baf3a0d23
qinweijunlin <qinweijunlin@qq.com>
2019-04-05 02:26:14 +0800

    发布

--- bin/phoneinit.sh
+++ bin/phoneinit.sh
@@ -4,9 +4,24 @@
 set -euo pipefail
 
 #### 常量 ####
+readonly E_NOT_FOUND=3
 
 #### 函数 ####
 
 #### 变量 ####
 
 #### 主体 ####
+if ! type adb; then
+  err "Command not found"
+  exit "${E_NOT_FOUND}"
+fi
+
+
+
+
+
+
+
+
+
+
