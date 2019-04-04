a2b008ffb6f80000ce926b72f625cc6768611300
qinweijunlin <qinweijunlin@qq.com>
2019-04-05 03:04:24 +0800

    发布

--- bin/phoneinit.sh
+++ bin/phoneinit.sh
@@ -4,11 +4,19 @@
 set -euo pipefail
 
 #### 常量 ####
+# 间隔时间: 分钟
+readonly INTERVAL_TIME=180
 readonly E_NOT_FOUND=3
 
 #### 函数 ####
 
 #### 变量 ####
+readonly phonestart="${tempdir}/.phonestart"
+starttime=0
+currenttime="$(date '+%s')"
+intervaltime="$((INTERVAL_TIME * 60))"
+readonly currenttime
+readonly intervaltime
 
 #### 主体 ####
 if ! type adb; then
@@ -16,12 +24,14 @@ if ! type adb; then
   exit "${E_NOT_FOUND}"
 fi
 
+if [[ -s "${phonestart}" ]]; then
+  starttime="$(cat "${phonestart}")"
+fi
 
-
-
-
-
-
-
-
-
+if (((currenttime - starttime) > intervaltime)); then
+  adb kill-server  || true
+  sleep 3
+  adb start-server || true
+  sleep 3
+  echo "${currenttime}" > "${phonestart}"
+fi
