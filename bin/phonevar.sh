a9df78152d07bc4eea7ae1cdf53de9579ca0a7e7
qinweijunlin <qinweijunlin@qq.com>
2019-04-08 12:53:41 +0800

    发布

--- bin/phonevar.sh
+++ bin/phonevar.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 #
-# 控制手机的起点
+# 常量与变量
 set -euo pipefail
 
-#### 常量 ####
+# 常量 - 文件与目录
 readonly ROOTDIR='/yzaj'
 
-#### 函数 ####
-err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
-}
-
-#### 变量 ####
+# 变量 - 文件与目录
 readonly bindir="${ROOTDIR}/phone/bin"
 readonly confdir="${ROOTDIR}/phone/conf"
 readonly tempdir="${ROOTDIR}/temp/phone"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### 变量 ####
+
 todaydir="${tempdir}/log/$(date +'%Y-%m-%d')"
 beforedir="${tempdir}/log/$(date +'%Y-%m-%d' -d '-7day')"
 readonly todaydir
 readonly beforedir
 phonelog="${todaydir}/$(date +'%H-%M-%S').log"
 readonly phonelog
-
-#### 主体 ####
-if [[ -d "${beforedir}" ]]; then
-  rm -r "${beforedir}"
-fi
-
-mkdir -p "${todaydir}"
-
-. "${bindir}"/phonemain.sh 2>&1 | tee "${phonelog}"
