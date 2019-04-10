ed09aaaaa1de5b52887f2f9791fcfda4c927e3aa
qinweijunlin <qinweijunlin@qq.com>
2019-04-10 18:24:15 +0800

    发布

--- bin/phonefunc.sh
+++ bin/phonefunc.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 
 ###################################################################################################
 #
+#       err_mo
+#       err_uo
+#       get_nums
+#
 #       
 #
 ###################################################################################################
@@ -13,19 +17,30 @@ set -euo pipefail
 # 使  用: 
 # 参数 1: 
 # 返回值: 
-# 备  注: 
+# 备  注: 该函数的使用范围受限
+err_mo() {
+  err "phone.sh: missing operand"
+  exit "${E_MISSING_OPERAND}"
+}
 
 # 功  能: 
 # 使  用: 
 # 参数 1: 
 # 返回值: 
-# 备  注: 
+# 备  注: 该函数的使用范围受限
+err_uo() {
+  err "phone.sh: unrecognized option '${phonearg}'"
+  exit "${E_UNRECOGNIZED_OPTION}"
+}
 
 # 功  能: 
 # 使  用: 
 # 参数 1: 
 # 返回值: 
-# 备  注: 
+# 备  注: 该函数的使用范围受限
+get_nums() {
+  phonenums="${phonenums}\n${phonenum}"
+}
 
 # 功  能: 
 # 使  用: 
