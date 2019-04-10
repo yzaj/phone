3fae2db7f687f826ddd85b06dbb242d77d09b728
qinweijunlin <qinweijunlin@qq.com>
2019-04-11 07:25:11 +0800

    发布

--- bin/phonevar.sh
+++ bin/phonevar.sh
@@ -16,9 +16,11 @@ interval_time=180
 readonly bindir="${ROOTDIR}/phone/bin"
 readonly confdir="${ROOTDIR}/phone/conf"
 readonly tempdir="${ROOTDIR}/temp/phone"
+
 todaydir="${tempdir}/log/$(date +'%Y-%m-%d')"
 beforedir="${tempdir}/log/$(date +'%Y-%m-%d' -d '-7day')"
 phonelog="${todaydir}/$(date +'%H-%M-%S').log"
+
 readonly todaydir
 readonly beforedir
 readonly phonelog
