84b5cc34102d1c32d8034272dca119c702814a1a
qinweijunlin <qinweijunlin@qq.com>
2019-04-10 12:43:05 +0800

    发布

--- bin/phonemain.sh
+++ bin/phonemain.sh
@@ -3,38 +3,8 @@
 # 主程序
 set -euo pipefail
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#### 常量 ####
-
-#### 包含 ####
-. "${confdir}"/phone.conf
 . "${bindir}"/phonelist.sh
+. "${bindir}"/phonefunc.sh
 . "${bindir}"/phonecmd.sh
 . "${bindir}"/phonearg.sh
 . "${bindir}"/phoneinit.sh
-
-#### 函数 ####
-
-#### 变量 ####
-
-#### 主体 ####
